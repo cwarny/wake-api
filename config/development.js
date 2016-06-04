50d243b98369e50b6954bd61eef5c7e2d6704be4
Cedric Warny <cedric.warny@gmail.com>
2016-06-03 23:21:12 -0400

    changed es url

--- config/development.js
+++ config/development.js
@@ -1,5 +1,5 @@
 module.exports = {
 	es: {
-		url: process.env.ES_URL_DEV || 'http://localhost:9200'
+		url: process.env.ES_URL_DEV || 'https://en99ganf:5jv55is26909k0ta@ash-9304498.us-east-1.bonsai.io'
 	}
 };
